c0c6fd4aec03cc78ec6d5bd69ec247e04480ada2
dangerbots <79362039+dangerbots@users.noreply.github.com>
2021-04-04 15:53:40 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -8,16 +8,16 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""[★](https://telegra.ph/file/740f87cbf8ef39d7af03d.png)I am **{bn}** A telegram voice chat bot for playing songs in your group voice chat
+        f"""[★](https://telegra.ph/file/8659dbdff98d2f4512b1f.jpg)I am **{bn}** A telegram voice chat bot for playing songs in your group voice chat
         """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "SOURCE CODE", url="https://github.com/zyrus-basi/vc_heroku"
+                        "SOURCE CODE", url="https://t.me/Miss_meenuus_bot"
                     ),
                     InlineKeyboardButton(
-                        "GROUP||COMMANDS", url="https://t.me/unitedbotsupport"
+                        "DEV", url="https://t.me/danger_of_telegram"
                     )
                 ]
             ]
